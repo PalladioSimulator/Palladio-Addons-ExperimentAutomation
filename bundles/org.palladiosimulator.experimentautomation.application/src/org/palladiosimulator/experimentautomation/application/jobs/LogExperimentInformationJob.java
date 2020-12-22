bd2af8915c7c290eec9555a9b31a8420c42cde75
Martina Rapp <rapp@fzi.de>
2020-12-22 15:58:54 +0100

    EXPAUTOM-9: added dedicated logger; replaced system.out.println statements with proper logging stmts

--- bundles/org.palladiosimulator.experimentautomation.application/src/org/palladiosimulator/experimentautomation/application/jobs/LogExperimentInformationJob.java
+++ bundles/org.palladiosimulator.experimentautomation.application/src/org/palladiosimulator/experimentautomation/application/jobs/LogExperimentInformationJob.java
@@ -2,6 +2,7 @@ package org.palladiosimulator.experimentautomation.application.jobs;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.palladiosimulator.experimentautomation.application.VariationFactorTuple;
 import org.palladiosimulator.experimentautomation.experiments.Experiment;
@@ -14,7 +15,7 @@ import de.uka.ipd.sdq.workflow.mdsd.blackboard.MDSDBlackboard;
 
 public class LogExperimentInformationJob extends SequentialBlackboardInteractingJob<MDSDBlackboard> {
 
-    // private static final Logger LOGGER = Logger.getLogger(LogExperimentInformationJob.class);
+    private static final Logger LOGGER = Logger.getLogger(LogExperimentInformationJob.class);
 
     private final Experiment experiment;
     private final AbstractSimulationConfig simulationConfig;
@@ -36,7 +37,6 @@ public class LogExperimentInformationJob extends SequentialBlackboardInteracting
      */
     @Override
     public void execute(final IProgressMonitor monitor) throws JobFailedException, UserCanceledException {
-        // if(LOGGER.isInfoEnabled()) {
         final StringBuilder stringBuilder = new StringBuilder();
 
         stringBuilder.append("\n");
@@ -57,9 +57,9 @@ public class LogExperimentInformationJob extends SequentialBlackboardInteracting
         }
 
         stringBuilder.append("REPETITION: ");
-        stringBuilder.append(this.repetition);
+        stringBuilder.append(String.valueOf(this.repetition));
         stringBuilder.append(" of ");
-        stringBuilder.append(this.experiment.getRepetitions());
+        stringBuilder.append(String.valueOf(this.experiment.getRepetitions()));
         stringBuilder.append("\n");
 
         stringBuilder.append("SIMULATOR ID: \"");
@@ -70,11 +70,8 @@ public class LogExperimentInformationJob extends SequentialBlackboardInteracting
         stringBuilder.append(this.simulationConfig.getRecorderName());
         stringBuilder.append("\"\n");
 
-        System.out.print(stringBuilder.toString());
 
-        // LOGGER.info(stringBuilder.toString());
-        // }
-
+         LOGGER.info(stringBuilder.toString());
         super.execute(monitor);
     }
 }
